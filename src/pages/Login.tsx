cd3af13ea8cf218f0e60ca0bd5aeaa3dd5800e39
rajksharma-dev <rajsharmafor4@gmail.com>
2026-03-05 18:28:18 +0530

    updated label

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -29,8 +29,9 @@ const Login = () => {
     <div className="flex items-center justify-center h-screen bg-gray-100">
       <div className="bg-white p-8 rounded-xl shadow-md w-96">
         <h2 className="text-2xl font-bold mb-6 text-center">
-          Venue Login - a one-stop shop
+          Venue Login
         </h2>
+        <h3 className="text-1xl font-bold mb-6 text-center">"One-Stop Shop"</h3>
 
         <input
           type="email"
